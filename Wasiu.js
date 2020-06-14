60a49708109930f382aaf0b62ab2d5aeb6a1ea6a
Abiola Wasiu <crownprincewadas@gmail.com>
2020-06-14 23:28:00 +0100

    Hello from the other side

---
+++ Wasiu.js
@@ -0,0 +1,9 @@
+function myDetails() {
+    var name = "[Abiola Wasiu]";
+    var id = "[HNG-06788]";
+    var language = "[Javascript]";
+    var email = "crownprincewadas@gmail.com"
+
+    console.log("Hello World, this is "+name+" with HNGi7 ID "+id+" using "+language+" for stage 2 task");
+}
+myDetails();
